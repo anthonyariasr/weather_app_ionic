cdcbc551fee9727c57cc013e4a80ee97c23d8352
AnthonyAriass <anthonyarias2000@estudiantec.cr>
2024-09-30 14:54:25 -0600

    weather app

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,14 +1,55 @@
+import React, { useState, useEffect } from 'react';
 import './ExploreContainer.css';
 
 interface ContainerProps {
-  name: string;
+  city: string; // Recibimos la ciudad como prop
 }
 
-const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
+const ExploreContainer: React.FC<ContainerProps> = ({ city }) => {
+  const [weatherData, setWeatherData] = useState<any>(null);
+  const [locationName, setLocationName] = useState<string>(city);
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      try {
+        const response = await fetch(`https://wttr.in/${city}?format=j1`);
+        const data = await response.json();
+        setWeatherData(data);
+        // Actualizar el nombre de la ciudad basado en la respuesta
+        setLocationName(data.nearest_area[0].areaName[0].value);
+      } catch (error) {
+        console.error('Error fetching weather data:', error);
+      }
+    };
+    fetchWeather();
+  }, [city]);
+
+  // Función para obtener un emoji según la condición climática
+  const getWeatherEmoji = (condition: string) => {
+    condition = condition.toLowerCase()
+    if (condition.includes('sunny')) return '⛅';
+    if (condition.includes('clear')) return '☀️';
+    if (condition.includes('cloudy') || condition.includes('drizzle')) return '☁️';
+    if (condition.includes('rain')) return '🌧️';
+    if (condition.includes('snow')) return '❄️';
+    if (condition.includes('thunderstorm')) return '⛈️';
+    if (condition.includes('fog') || condition.includes('Mist')) return '🌫️';
+    return '🌡️'; // Emoji por defecto si no hay coincidencia
+  };
+
   return (
     <div className="container">
-      <strong>{name}</strong>
-      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      {weatherData ? (
+        <div>
+          {/* Mostrar el emoji correspondiente a la condición climática con tamaño más grande */}
+          <div style={{ fontSize: '4rem' }}>{getWeatherEmoji(weatherData.current_condition[0].weatherDesc[0].value)}</div>
+          <h2>Weather in {locationName}</h2>
+          <p>Codition: {weatherData.current_condition[0].weatherDesc[0].value}</p>
+          <p>Temperature: {weatherData.current_condition[0].temp_C}°C</p>
+        </div>
+      ) : (
+        <p>Cargando clima...</p>
+      )}
     </div>
   );
 };
